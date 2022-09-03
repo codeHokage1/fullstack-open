d5693f14ef4a74d395c13da996c12376e1887708
Sodiq Farhan A <sodiqfarhan360@gmail.com>
2022-09-03 17:22:48 +0100

    Do step3 in courseInfo

--- part1/courseInfo/src/App.js
+++ part1/courseInfo/src/App.js
@@ -39,12 +39,18 @@ const Total = (props) => {
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    excercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    excercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    excercises: 14
+  }
 
   return (
     <div>

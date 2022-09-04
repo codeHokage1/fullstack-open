5b60311a1f89aaba287e591afd5404a4d623584a
Sodiq Farhan A <sodiqfarhan360@gmail.com>
2022-09-04 19:20:29 +0100

    Add step1 of unicafe exercises of part1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,10 +4,25 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
 
   return (
     <div>
+      <h1>Give Feedback</h1>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      
+
+
     </div>
   );
 }

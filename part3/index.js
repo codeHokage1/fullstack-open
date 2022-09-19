55562dc2270b6be802fe67f7ee30ea9550c5c405
Sodiq Farhan <sodiqfarhan360@gmail.com>
2022-09-19 23:14:44 +0100

    Refactor the simple web server to use Express

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,52 @@
-const http = require('http');
+// ########## WITHOUT EXPRESS ##############
+// #########################################
+
+// const http = require('http');
+
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       date: "2022-05-30T17:30:31.098Z",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only Javascript",
+//       date: "2022-05-30T18:39:34.091Z",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       date: "2022-05-30T19:20:14.298Z",
+//       important: true
+//     }
+// ]
+
+
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     res.end(JSON.stringify(notes));
+// })
+
+// const PORT = 3000;
+// app.listen(PORT);
+// console.log('Server running on port ', PORT);
+
+
+
+// #########################################
+// #########################################
+// #########################################
+
+
+
+
+// ########## WITH EXPRESS ##############
+// #########################################
+const express = require('express');
+const app = express();
 
 let notes = [
     {
@@ -21,12 +69,14 @@ let notes = [
     }
 ]
 
-
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(notes));
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to Notes Server</h1>")
+})
+app.get("/notes", (req, res) => {
+    res.json(notes);
 })
 
-const PORT = 3000;
-app.listen(PORT);
-console.log('Server running on port ', PORT);
+const PORT = 3070;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
